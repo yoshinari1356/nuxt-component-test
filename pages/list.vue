157ccf670628cf1c8f87fc485b7ff99b0147857d
yoshinari nihei <n0802jp2@gmail.com>
2020-05-05 00:01:10 +0900

    add head

--- pages/list.vue
+++ pages/list.vue
@@ -48,6 +48,21 @@ export default {
       items3: []
     }
   },
+  head() {
+    // このページ向けにメタタグを設定します
+    return {
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ],
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css?family=Roboto'
+        }
+      ]
+    }
+  },
   }
 }
 </script>
