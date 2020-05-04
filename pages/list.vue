c221be1caf5d3584a5d3aa101b7acfa4debc9ccd
yoshinari nihei <n0802jp2@gmail.com>
2020-05-04 23:24:57 +0900

    asyncData axios get items

--- pages/list.vue
+++ pages/list.vue
@@ -7,14 +7,27 @@
         <listItem :item="item"></listItem>
       </v-flex>
     </v-layout>
+    <h2>item2 asyncDataで先に読み込む</h2>
+    <v-layout v-for="(item, i) in items2" :key="i">
+      <v-flex>
+        <listItem :item="item"></listItem>
+      </v-flex>
+    </v-layout>
   </div>
 </template>
 
 <script>
+import $axios from 'axios'
 import listItem from '~/components/listItem'
 export default {
   components: {
     listItem
+  },
+  asyncData() {
+    return $axios.get(`http://localhost:3004/users`).then((res) => {
+      const items2 = res.data
+      return { items2 }
+    })
   },
   data() {
     return { items: [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }] }
